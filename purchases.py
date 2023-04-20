c74cd780e7bd9f97d2da93a09a34121c91a26f73
rashid <ri-post@yandex.ru>
2023-04-20 16:01:05 +0500

    Сдеал все задачи по pandas

--- purchases.py
+++ purchases.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 import pandas as pd
 
-base_dir = Path('data')
+target_dir = Path('data')
 
 
-def get_total_df():
+def get_total_df(target_dir: Path) -> pd.DataFrame:
     all_dataframes = []
-    for date_dir in base_dir.iterdir():
+    for date_dir in target_dir.iterdir():
         if date_dir.is_file():
             continue
         date = datetime.datetime.strptime(str(date_dir.name), '%Y-%m-%d')
@@ -27,4 +27,37 @@ def get_total_df():
     return pd.concat(all_dataframes)
 
 
-print(get_total_df())
+def main():
+    total_dataframe = get_total_df(target_dir)
+    print('Задание 1')
+    print('Общий датафрейм:')
+    print(total_dataframe)
+
+    # total_dataframe.to_csv('total_dataframe.csv')
+
+    print('\nЗадание 2')
+    print('Сумма по колонке quantity:', total_dataframe['quantity'].sum())
+
+    top_users = (total_dataframe.groupby(['name'])['quantity'].sum()
+                 .sort_values(ascending=False))
+    if top_users.empty:
+        names_string = 'Датафрейм пуст'
+    else:
+        max_sum = top_users.values[0]
+        top_names = [
+            name for name, value in top_users.items() if value == max_sum
+        ]
+        top_names.sort()
+        names_string = ', '.join(top_names)
+    print('\nЗадание 3')
+    print('Больше всех товаров купил пользователь(и):', names_string)
+
+    top_10 = (total_dataframe.groupby(['product_id'])['quantity'].sum()
+              .sort_values(ascending=False).head(10))
+    print('\nЗадание 4')
+    print('Топ-10 товаров по числу проданных единиц за всё время:',
+          list(top_10.keys()))
+
+
+if __name__ == '__main__':
+    main()
